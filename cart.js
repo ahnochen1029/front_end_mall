9f5431da531fe3684d4294209b17b9e7eca539d9
AhnoChen <ahnochen@wistronits.com>
2023-01-31 16:48:06 +0800

    add change qty feat

--- cart.js
+++ cart.js
@@ -5,12 +5,24 @@ const renderCart = (data) => {
     let i = 1;
     let totalPrice = 0;
     data?.forEach(element => {
+        let optionHTML = '';
+        for (let j = 1; j < 6; j++) {
+            if (j !== element.quantity) {
+                optionHTML += `
+                <option value="${j}">${j}</option>
+                `;
+            }
+        }
         rawHTML += `
         <tr>
             <th scope="row">${i}</th>
             <td>${element.productName}</td>
-            <td>${element.descprition || ''}</td>
-            <td>${element.quantity}</td>
+            <td>
+            <select class="custom-select" id="cart-change-quantity-${element.productId}" data-id="${element.productId}"required>
+                <option selected disabled value="${element.quantity}">${element.quantity}</option>
+                ${optionHTML}
+            </select>
+            </td>
             <td>${element.price}</td>
             <td>${element.price * element.quantity}</td>
             <td>
@@ -23,9 +35,31 @@ const renderCart = (data) => {
     cartBody.innerHTML = rawHTML;
 };
 
+// 更新購物車項目數量
+function changeQtyFromCart(id, value) {
+    const cartData = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
+    if (cartData.length == 0) return;
+
+    const item = cartData.find((element) => element.productId === id);
+
+    item.quantity = value;
+
+    //存回 local storage
+    localStorage.setItem('cart', JSON.stringify(cartData));
+
+}
+
 cartBody.addEventListener('click', function onPanelClicked(event) {
     const id = event.target.dataset.id;
-    removeFromCart(Number(id));
+    if (event.target.matches('.btn-remove-item')) {
+        removeFromCart(Number(id));
+    } else if (event.target.matches('.custom-select')) {
+        console.log('select.....');
+        const changeQuantity = document.getElementById(`cart-change-quantity-${id}`);
+        const value = changeQuantity.value;
+        console.log('value=>', value);
+        changeQtyFromCart(Number(id), Number(value));
+    }
 });
 
 // 刪除購物車項目
@@ -33,11 +67,11 @@ function removeFromCart(id) {
     const cartData = localStorage.getItem("cart") ? JSON.parse(localStorage.getItem("cart")) : [];
     if (cartData.length == 0) return;
 
-    const movieIndex = cartData.findIndex((element) => element.productId === id);
-    if (movieIndex === -1) return;
+    const itemIndex = cartData.findIndex((element) => element.productId === id);
+    if (itemIndex === -1) return;
 
     //刪除該項目
-    cartData.splice(movieIndex, 1);
+    cartData.splice(itemIndex, 1);
 
     //存回 local storage
     localStorage.setItem('cart', JSON.stringify(cartData));
